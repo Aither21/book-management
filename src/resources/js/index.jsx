f059090fd6b098e84ec7d4c273886c95dd86f627
Kouki Takano <77576736+Aither21@users.noreply.github.com>
2022-12-18 19:56:56 +0900

    Revert "Feature/screen pre user register"

--- src/resources/js/index.jsx
+++
@@ -1,1 +0,0 @@
-export { default as PreUserRegisterForm } from './pages/PreUserRegisterForm.jsx'
--- src/resources/js/pages/pre_user_register_form.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { default as MailForm } from '../components/mail_form';
-
-const PreUserRegisterForm = (props) => {
-    return(
-			<MailForm />
-    );
-  }
-export { PreUserRegisterForm };
