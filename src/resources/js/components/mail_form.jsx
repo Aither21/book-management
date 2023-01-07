a32f4250d1890cbc991c09897645cc659e51942a
Kouki <sanada21yukimura@gmail.com>
2023-01-07 14:51:30 +0900

    管理画面(貸出、返却、全図書)、サイドパネル(仮)、ログインと仮会員のレイアウト

--- src/resources/js/components/mail_form.jsx
+++ src/resources/js/components/mail_form.jsx
@@ -40,7 +40,7 @@ const MailForm = (props) => {
 			.then((response) => {
 				const statusCode = response.status;
 				console.log(statusCode);
-				if(statusCode === 201){
+				if(statusCode === 201||statusCode === 200){
 					setSendState(true);
 					return;
 				}
@@ -56,10 +56,63 @@ const MailForm = (props) => {
 		<>
 		{
 			SendState === false ?
-			<>
-			<p>仮登録情報を入力してください</p>
-			<p className="text-red-500">{validateError}</p>
-			<p className="w-44 mt-2">名前：</p>
+
+			<div>
+				<section className="text-gray-600 body-font">
+					<div>仮登録情報を入力してください</div>
+					<div className="text-red-500">{validateError}</div>
+					<div className="container px-5 py-5 mx-auto flex flex-wrap items-center">
+						<div className="bg-gray-100 rounded-lg p-8 flex flex-col w-full">
+							<div className="relative mb-4">
+								<label htmlFor="email" className="leading-7 text-sm text-gray-600">
+									名前
+								</label>
+								<input type="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+									onChange={(event) => changeName(event)}>
+								</input>
+							</div>
+							<div className="relative mb-4">
+								<label htmlFor="email" className="leading-7 text-sm text-gray-600">
+									メールアドレス
+								</label>
+								<input type="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+									onChange={(event) => changeMail(event)}>
+								</input>
+							</div>
+							<div className="relative mb-4">
+								<label htmlFor="password" className="leading-7 text-sm text-gray-600">
+									パスワード
+								</label>
+								<input type="text" name="password" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+									onChange={(event) => changePassword(event)}>
+								</input>
+							</div>
+							<div className="relative mb-4">
+								<label htmlFor="password" className="leading-7 text-sm text-gray-600">
+									パスワード（確認）
+								</label>
+								<input type="text" name="password" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+									onChange={(event) => changePasswordConfirmation(event)}>
+								</input>
+							</div>
+							<button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+								onClick={() => registerRequest(nameState, mailState, passwordState, passwordConfirmationState)}>
+								仮登録する
+							</button>
+						</div>
+					</div>
+				</section>
+			</div>
+			:
+			<div>
+			<p>仮登録が完了しました</p>
+			<p>受信したURLから本登録を完了してください！</p>
+			</div>
+			}
+
+
+
+			{/* <p className="w-44 mt-2">名前：</p>
 			<input width={50}
 			type="text" name="name" className="border" onChange={(event) => changeName(event)}></input>
 			
@@ -74,64 +127,9 @@ const MailForm = (props) => {
 			
 			<button className="mt-7 border p-4 cursor-pointer"
 			onClick={() => registerRequest(nameState, mailState, passwordState, passwordConfirmationState)}>仮登録する</button>
-			</>
-			:
-			<>
-			<p>仮登録できました</p>
-			<p>受信したURLから本登録を完了してください！</p>
-			</>
-		}
+		</> */}
 		</>
 	)
 }
-
-// const instance = axios.create({
-// 	method: 'GET',
-// 	baseURL: "/api/user",
-// });
-
-// async function fetchData() {
-	// 	let response = await axios.post("/api/register", {
-	// 		name: "しろたん",
-	// 		email: "shirotan@gmail.com",
-	// 		password: "password",
-	// 		password_confirmation: "password"
-	// 	});
-	// 	return response;
-	// }
-// const validate = (mail) => {
-// 	const mailInputElement = document.querySelector('.mail');
-// 	setMailState(mailInputElement.value);
-// 	if(mailState.length < 1){
-// 		setValidateError('2文字以上で入力してください');
-// 	}
-// 	else {
-// 		setValidateError('');
-// 	}
-// }
-
-
-
-// const request = () => {
-// 	const requestOptions = {
-// 		method: 'GET',
-// 		headers:{'Content-Type': 'application/json'},
-// 		body: JSON.stringify('あ')
-// 	}
-// 	const url = '/api/user';//リクエスト先のURL
-// 	const result = fetch(url,requestOptions);
-// 	console.log(result);
-// }
-
-// const request = (mail) => {
-// 	const requestOptions = {
-// 		method: 'GET',
-// 		headers:{'Content-Type': 'application/json'},
-// 		body: JSON.stringify({mail: {mail}})//入力されたメールアドレス
-// 	}
-// 	const url = '/api/user';//リクエスト先のURL
-// 	const result = fetch(url,requestOptions);
-// 	console.log(result);
-// }
 
 export default MailForm;
--- src/resources/js/pages/mail_send.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { default as MailSend } from '../components/mail_form';
-
-const MailSend = (props) => {
-	return(
-		<MailSend />
-	);
-}
-export { MailSend };
